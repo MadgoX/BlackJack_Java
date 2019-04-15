1097e151afbbdcf2fc32360c2e18d29a6c4bc5b2
Lucien <lbouyeur@gmail.com>
2019-04-15 12:24:03 +0200

    Programme commenté

--- Card.java
+++ Card.java
@@ -1,11 +1,17 @@
 class Card {
-
+   /*
+   * L'enum "Suit". C'est une énumération qui permet de déterminer la couleur
+   * d'une carte. Pour utiliser le type "Suit" dans un autre fichier il faut faire
+   * "Card.Suit.HEART" par exemple si on veut utiliser le coeur.
+   */
    public enum Suit {
       HEART("Heart"),
       DIAMOND("Diamond"),
       SPADE("Spade"),
       CLUB("Club");
 
+      // suitName qui permet de l'afficher en redéfinissant toString, de sorte à
+      // ce que quand on fait un system.out.println ça affiche la couleur et non une variable chelou.
       private String suitName = "";
 
       Suit(String suitName) {
@@ -16,11 +22,15 @@ class Card {
          return suitName;
       }
    }
+   //Un tableau de chaines de caractères pour les noms des valeurs
    public static final String[] RANK_NAMES = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+   //Trois variables pour couleur, rang et valeur, ici c'est la valeur au blackjack qui nous intéresse
    private Suit suit;
    private int rank;
    private int value;
 
+   //Le constructeur de la classe dans lequel on initialise nos variables à leur valeur en paramètre
+   //On définit également leur valeur par rapport à leur rang
    public Card(Suit suit, int rank) {
       this.suit = suit;
       this.rank = rank;
@@ -31,26 +41,33 @@ class Card {
       }
    }
 
+   //Le getter de couleur, permet de retourner une couleur de type Suit
    public Suit getSuit() {
       return this.suit;
    }
 
+   //Le getter de rang, retourne un int
    public int getRank() {
       return this.rank;
    }
 
+   //Le getter de nom de couleur si on veut afficher ailleurs
    public String getSuitName() {
       return this.suit.toString();
    }
 
+   //Le getter de nom de rang pour afficher ailleurs
    public String getRankName() {
       return RANK_NAMES[rank];
    }
 
+   //La redéfinition de la méthode toString qui permet d'afficher une carte
+   //avec System.out.println ailleurs
    public String toString() {
       return ""+this.getRankName()+" of "+this.getSuitName();
    }
 
+   //Le getter pour la valeur de la carte
    public int getValue() {
       return this.value;
    }
--- Deck.java
+++ Deck.java
@@ -1,12 +1,26 @@
 import java.util.*;
 
+//La classe Deck, qui consiste en un et un seul tableau de cartes.
 class Deck {
+   //Le tableau de cartes, donc notre deck
    private static ArrayList<Card> deck;
+   //Le singleton est un peu compliqué. En gros c'est un design pattern qui permet
+   //D'éviter qu'une classe ne crée plus d'un seul objet. Pour ça, on crée une
+   //variable statique singleton initialisée à null et qui prend une valeur si
+   //il n'existe pas d'autre objet de type Deck. Pour construire qu'un seul Deck,
+   //On laisse le constructeur vide et on appelle la méthode getInstnce() à chaque
+   //fois qu'on veut créer un deck, de sorte à vérifier avant si un deck n'a pas
+   //déjà été créé.
    private static Deck singleton = null;
+   //Pour suit ça sert simplement à l'initialisation du deck
    private Card.Suit suit = Card.Suit.HEART;
 
+   //La classe est instanciable donc elle a un constructeur mais il est vide car
+   //elle ne doit créer qu'un seul objet (cherche design pattern singleton si tu veux)
    private Deck(){}
 
+   //Le nouveau constructeur de la classe. Il vérifie qu'on n'a pas créé d'autre
+   //objet Deck avant d'initialiser la variable.
    public static Deck getInstance() {
       if (singleton == null){
          singleton = new Deck();
@@ -15,6 +29,7 @@ class Deck {
       return singleton;
    }
 
+   //La classe initialize_deck permet de le créer avec les cartes dans l'ordre.
    public void initialize_Deck() {
       for(int i = 0; i<13; i++) {
          deck.add(new Card(suit, i));
@@ -33,16 +48,20 @@ class Deck {
       }
    }
 
+   //La classe print_Deck permet de l'afficher quand on le demande.
    public void print_Deck() {
       for(int i = 0; i<52; i++) {
          System.out.println(deck.get(i));
       }
    }
 
+   //La classe shuffle permet de le mélanger et c'est super pratique que la
+   //méthode shuffle existe déjà dans ArrayList
    public void shuffle() {
       Collections.shuffle(deck);
    }
 
+   //La méthode top permet de récupérer la première carte du paquet
    public Card top() {
       return deck.remove(0);
    }
